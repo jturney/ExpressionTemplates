ec0887711d28ca501bb48e681a553fb99ef5bc05
Justin Turney <justin.turney@gmail.com>
2012-12-04 11:47:41 -0500

    Adds implementation of is_expression type trait checker.

--- include/et/typetraits/is_expression.h
+++ include/et/typetraits/is_expression.h
@@ -1,4 +1,17 @@
-#ifndef IS_EXPRESSION_H
-#define IS_EXPRESSION_H
+#if !defined is_expression_h
+#define is_expression_h
 
-#endif // IS_EXPRESSION_H
+#include <boost/type_traits/is_base_of.hpp>
+
+namespace et {
+
+struct expression;
+
+template<typename T>
+struct is_expression
+{
+    enum { value = boost::is_base_of<expression, T>::value && !boost::is_base_of<T, expression>::value };
+};
+
+}
+#endif // is_expression_h
