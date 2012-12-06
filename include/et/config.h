7550ef3f366b10d69c9315c4823c4f5dcc5ea9a4
Justin Turney <justin.turney@gmail.com>
2012-12-06 07:24:20 -0500

    Updates config.h to handle setting mode flags and including proper headers for intrinsics

--- include/et/config.h
+++ include/et/config.h
@@ -6,12 +6,46 @@
 #   define alignas(x)
 #endif
 
-#if defined __SSE2__
-#   include <emmintrin.h>
+#if defined(__SSE__)
+#   include <xmmintrin.h>
+#   define ET_SSE_MODE 1
+#else
+#   define ET_SSE_MODE 0
 #endif
 
-#if defined __AVX__
+#if defined(__SSE2__)
+#   include <emmintrin.h>
+#   define ET_SSE2_MODE 1
+#else
+#   define ET_SSE2_MODE 0
+#endif
+
+#if defined(__SSE3__)
+#   include <pmmintrin.h>
+#   define ET_SSE3_MODE 1
+#else
+#   define ET_SSE3_MODE 0
+#endif
+
+#if defined(__SSSE3__)
+#   include <tmmintrin.h>
+#   define ET_SSSE3_MODE 1
+#else
+#   define ET_SSSE3_MODE 0
+#endif
+
+#if defined(__SSE4_2__) || defined(__SSE4_1__)
+#   include <smmintrin.h>
+#   define ET_SSE4_MODE 1
+#else
+#   define ET_SSE4_MODE 0
+#endif
+
+#if defined(__AVX__)
 #   include <immintrin.h>
+#   define ET_AVX_MODE 1
+#else
+#   define ET_AVX_MODE 0
 #endif
 
 #endif // config_h
